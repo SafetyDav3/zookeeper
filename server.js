ced09c11d02743159a41c95628978d3222cbc847
SafteyDav3 <talktomedw@gmail.com>
2022-05-14 09:37:41 -0500

    Heroku and params

--- server.js
+++ server.js
@@ -50,6 +50,11 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+}
+
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
@@ -57,6 +62,16 @@ app.get("/api/animals", (req, res) => {
   }
   res.json(results);
 });
+
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
