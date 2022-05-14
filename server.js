4ccd41ddd97c9cec43b71a27052936a6197f5fb4
SafteyDav3 <talktomedw@gmail.com>
2022-05-14 15:25:11 -0500

    Post and get

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 const PORT = process.env.PORT || 3001;
+const fs = require("fs");
+const path = require("path");
 
 const express = require("express");
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 const { animals } = require("./data/animals.json");
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== "string") {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== "string") {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== "string") {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -54,6 +77,15 @@ function findById(id, animalsArray) {
   const result = animalsArray.filter((animal) => animal.id === id)[0];
   return result;
 }
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, "./data/animals.json"),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
+}
 
 app.get("/api/animals", (req, res) => {
   let results = animals;
@@ -72,6 +104,23 @@ app.get("/api/animals/:id", (req, res) => {
   }
 });
 
+app.post("/api/animals", (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formatted.");
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
+
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+  console.log(
+`↓↓↓↓↓↓
+→→→→→→→→→→→→→   http://localhost:${PORT}/api/animals is now live!
+↑↑↑↑↑↑`
+  );
 });
