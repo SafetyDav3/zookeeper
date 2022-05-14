a9cd90476e3ec5595f83d9100ed91f0283f3d3aa
SafteyDav3 <talktomedw@gmail.com>
2022-05-14 08:50:25 -0500

    PORT

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const PORT = process.env.PORT || 3001;
+
 const express = require("express");
 const app = express();
 
@@ -55,6 +57,6 @@ app.get("/api/animals", (req, res) => {
   }
   res.json(results);
 });
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
